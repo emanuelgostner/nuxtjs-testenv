d9ac630351c3d3db3a46166c5b5a132402527079
Emanuel <emanuel.gostner@strabag.com>
2021-03-17 12:04:03 +0100

    heroku

--- Services/EventService.js
+++ Services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3001`,
+  baseURL: `http://my-json-server.typicode.com/emanuelgostner/nuxtjs-testenv`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
